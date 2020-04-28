77b7159c11785ab0f06fdf73a321f2f2231d9d71
Usman <usman1kadir@gmail.com>
2020-04-28 02:17:26 +0000

    ErrorFixing

--- MultiwayTrieSet.cpp
+++ MultiwayTrieSet.cpp
@@ -5,7 +5,8 @@
  */
 MultiwayTrieSet::MultiwayTrieSet() {
     /* YOUR CODE HERE */
-    root = new Node();
+    //cerr << "construcor";
+    this->root = new Node();
     numElements = 0;
 }
 
@@ -26,25 +27,36 @@ unsigned int MultiwayTrieSet::size() {
 
 void MultiwayTrieSet::insert(string s) {
     /* YOUR CODE HERE */
+    //cerr << "insertv :" << s;
     Node* curr = root;
+    //cerr << "curr :" << curr;
+
     for(unsigned int i = 0; i < s.length(); i++) {
+
         char letter = s[i];
-        cout << "Getshere";
+        //cout << "Gets here " << letter;
+        
+        if(curr->children[letter] == NULL) {
+            curr->children[letter] = new Node();
+        }
         curr = curr->children[letter];
+        //cout << "letter=" << letter << "curr =" << curr;
     }
     if(curr->isWord)
         return;
-    
-    curr->isWord = false;
+
+    curr->isWord = true;
     numElements++;
+    //cout << "numelements =" << numElements;
 }
 
 void MultiwayTrieSet::remove(const string & s) {
     /* YOUR CODE HERE */
+    //cerr << "remove";
     Node* curr = root;
     for(unsigned int i = 0; i < s.length(); i++) {
         char letter = s[i];
-        if(curr->children.find(letter) != curr->children.end()) {
+        if(curr->children.find(letter) == curr->children.end()) {
             return;
         }
         curr = curr->children[letter];
@@ -58,10 +70,17 @@ void MultiwayTrieSet::remove(const string & s) {
 
 bool MultiwayTrieSet::find(const string & s) {
     /* YOUR CODE HERE */
+    /*cout<< "---------";*/
     Node* curr = root;
+    /*if(curr == NULL) {
+        cout << "NULL";
+        return false;
+    }*/
+        
+
     for(unsigned int i = 0; i < s.length(); i++) {
         char letter = s[i];
-        if(curr->children.find(letter) != curr->children.end()) {
+        if(curr->children.find(letter) == curr->children.end()) {
             return false;
         }
         curr = curr->children[letter];
